a5293ce569e28783300c2e10175cb2eb98825f67
Nhu Quan (Jim) Tran <quantrannhu@gmail.com>
2023-05-31 05:31:49 +0000

    Issue #2 on going

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Issue #1:create an entry
+
 // input event listener to update photo URL
 const $inputPhotoURL = document.querySelector('#photoURL');
 const $entryImage = document.querySelector('.entry-image');
@@ -25,3 +27,53 @@ function formSave(event) {
   $form.reset();
 }
 $form.addEventListener('submit', formSave);
+
+// Issue #2: View the entry
+function renderEntry(entry) {
+  const $liEntry = document.createElement('li');
+  $liEntry.className = 'entry';
+  $liEntry.setAttribute('data-entry-id', entry.entryId);
+
+  const $divRow = document.createElement('div');
+  $divRow.className = 'row';
+  $liEntry.append($divRow);
+
+  const $divColumnHalf1 = document.createElement('div');
+  $divColumnHalf1.className = 'column-half';
+  $divRow.append($divColumnHalf1);
+
+  const $imgViewEntryImage = document.createElement('img');
+  $imgViewEntryImage.className = 'view-entry-image';
+  $imgViewEntryImage.setAttribute('src', entry.photoURL);
+  $imgViewEntryImage.setAttribute('alt', 'Entry Image');
+  $divColumnHalf1.append($imgViewEntryImage);
+
+  const $divColumnHalf2 = document.createElement('div');
+  $divColumnHalf2.className = 'column-half';
+  $divRow.append($divColumnHalf2);
+
+  const $h3EntryTitle = document.createElement('h3');
+  $h3EntryTitle.className = 'entry-title';
+  $h3EntryTitle.innerText = entry.title;
+  $divColumnHalf2.append($h3EntryTitle);
+
+  const $pEntryNotes = document.createElement('p');
+  $pEntryNotes.className = 'entry-notes';
+  $pEntryNotes.innerText = entry.notes;
+  $divColumnHalf2.append($pEntryNotes);
+
+  return $liEntry;
+}
+
+const $ulEntryList = document.querySelector('ul.entry-list');
+function DOMContentLoadedHandler(event) {
+  for (let i = 0; i < data.entries.length; i++) {
+    const $liEntry = renderEntry(data.entries[i]);
+    $ulEntryList.append($liEntry);
+  }
+}
+document.addEventListener('DOMContentLoaded', DOMContentLoadedHandler);
+
+// function toggleNoEntries(pNoEntry) {
+//   pNoEntry.classList.toggle('hidden');
+// }
